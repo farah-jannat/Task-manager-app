88e09a907c57f536fd3d39b713526047e33c8d0c
“farah-jannat” <farahjannatmithila@gmail.com>
2024-12-01 00:19:01 +0600

    feat : add task-router

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 require ('dotenv').config();
 require('./models/db');
+const TaskRouter = require('./routes/TaskRouter')
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
 
+app.use(bodyParser.json());
+app.use('/tasks', TaskRouter);
 
 app.listen(PORT, ()=>{
     console.log(`server is running on port = ${PORT}`);
--- routes/TaskRouter.js
+++ routes/TaskRouter.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
-
+const {createTask} = require('../controllers/TaskController')
 // to get all the task
 
 router.get('/',(req,res)=>{
     res.send('All tasks')
 })
+
+// to create a task
+router.post('/', createTask);
 
 module.exports = router;

6e6bfdf77fe1647fc0939e0f66bd4758f78fb46a
“farah-jannat” <farahjannatmithila@gmail.com>
2024-12-01 12:05:32 +0600

    feat: add cors

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ require('./models/db');
 const TaskRouter = require('./routes/TaskRouter')
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8080;
+const cors = require('cors')
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use('/tasks', TaskRouter);
+
 
 app.listen(PORT, ()=>{
     console.log(`server is running on port = ${PORT}`);

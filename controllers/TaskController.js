4238eb72c95419e700f8b318de4f08895c6638af
“farah-jannat” <farahjannatmithila@gmail.com>
2024-12-01 11:41:34 +0600

    feat: add update-task

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -5,7 +5,7 @@ const createTask  = async(req,res)=>{
     try{
         const model = new TaskModel(data)
         await model.save();
-        res.status(201)
+        res.status(200)
             .json({message:'task is created', success: true});
     }catch(err){
         res.status(500).json({message:'failed to create task', success:false});
@@ -15,14 +15,28 @@ const createTask  = async(req,res)=>{
 const fetchAllTask = async(req,res)=>{
     try{
         const data = await TaskModel.find({})
-        res.status(201)
+        res.status(200)
         .json({message:'All tasks', success:true, data})
     }catch(err){
         res.status(500).json({message:'failed to bring task', success: false})
     }
 }
 
+const updateTaskById = async(req,res)=>{
+    try{
+        const id = req.params.id;
+        const body = req.body;
+        const obj = {$set: {...body}};
+        await TaskModel.findByIdAndUpdate(id,obj);
+        res.status(200)
+        .json({message:'task updated', success:true});
+    }catch(err){
+        res.status(500).json({message:"failed to update task", success:false});
+    }
+}
+
 module.exports = {
     createTask,
-    fetchAllTask
+    fetchAllTask,
+    updateTaskById
 }
--- routes/TaskRouter.js
+++ routes/TaskRouter.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
-const {createTask, fetchAllTask} = require('../controllers/TaskController')
+const {createTask, fetchAllTask, updateTaskById} = require('../controllers/TaskController')
 // to get all the task
 
 router.get('/', fetchAllTask);
 
 // to create a task
 router.post('/', createTask);
+
+router.put('/:id', updateTaskById);
 
 module.exports = router;
